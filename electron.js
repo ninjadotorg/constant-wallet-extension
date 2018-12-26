81ced30fe5785aa46da534767dda30f1f863e025
0xsirrush <thaibao@autonomous.nyc>
2018-12-26 10:19:55 +0700

    - update electron

--- electron.js
+++ electron.js
@@ -1,13 +1,13 @@
 const electron = require('electron');
 const path = require('path');
 const fs = require('fs');
-const { download } = require('electron-dl');
-const { exec } = require('child_process');
+const {download} = require('electron-dl');
+const {exec} = require('child_process');
 
 
-const { app, BrowserWindow, Menu, ipcMain } = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
-const downloadLink = 'https://github.com/ninjadotorg/constant/releases/download/constant-0.0.1-beta/constant';
+const downloadLink = 'https://github.com/ninjadotorg/constant/releases/download/constant-0.0.2-beta/constant';
 
 const menu = new Menu();
 
@@ -48,7 +48,7 @@ function runChain() {
 
   fs.copyFileSync(path.relative(__dirname, 'wallet'), path.resolve(storePath, 'data/node-1/testnet/wallet'));
 
-  exec(`${path.resolve(storePath, 'constant')} --enablewallet --wallet "wallet" --walletpassphrase "12345678" --testnet --norpcauth --generate --producerkeyset "12pmcvmL2rYC6D4ydq5Ppv4xseoCcoeueb9y52H6EdTQLnXgJRYzc38T9cApymJQPkTrwqoeXh4Kk4A64miN1TygFth7UjFtnTyUtYAbZuvYR372Pkqs1yaEa1n5NPWbZNhDKsHLfaAEpy91CSRDzngJDABu6qmri8NvEnkYdKht2qF4dh7D9fcrihgwbKpmfLtj7tNcDZ8vaovMTZTj7pgpcavqwz7K3izB5prdsJt4jq8mGGQNJDR1PhkJWPeu7Rd41GoxvHit2NtA9egFUA444Tdco65GumDJPyeRN2K512eSCCGCiKQ9t9P7shq6vw22xmLHDA6xWBzSN2GBMGnnnawSPWe3upEehQunwvJtdg7J8j7JP7J2DZjQdSz9d3mL5tKiQdBPYwj3sLvNdLLFPXUfEQjMgJkiFDLDxkRfEKRkexhF31wJHxN3Qt3Mjeh9ou2UpUBEpoWFoqi6zyTdFbrmEJqvJK2Z8jCx1mxmQ97Naq94qvxuVJURsRT5FnV4oDS9tY4daS6Ae4GZywLe56LqXoS15hMi4KbssMcuzXBeQujdbXVhqVoZSv5gPh6XtWrhqBV1V6EpRFht9a9YyfxKq8TY35QPfapS8DRYuXYktpNcHfmWNMepMwU9Lw9Roj481qyqGa1xidWrcAXKzzTEokHZnLJRkXVnzBQ7uVB9Kgh8tf5vupP5R2ebrwc5E6Piw1QJJGboPb2uEva4bgngn5ghibtbBC9yBVJaCuPjkjQTZe3t6yoHmtUYomF9DNT6" --datadir "${path.resolve(storePath, 'data/node-1')}"`, (error, stdout, stderr) => {
+  exec(`${path.resolve(storePath, 'constant')} --enablewallet --wallet "wallet" --walletpassphrase "12345678" --testnet --norpcauth`, (error, stdout, stderr) => {
     if (error) {
       runChain();
     }
@@ -65,7 +65,7 @@ function createWindow() {
   win.maximize();
   win.setSize(414, win.getBounds().height);
   win.setResizable(false);
-  const { width } = electron.screen.getPrimaryDisplay().bounds;
+  const {width} = electron.screen.getPrimaryDisplay().bounds;
   win.setPosition(width - 414, 0);
   win.setMenu(null);
 
